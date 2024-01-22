5ae4391393c3abe302f9d04be2b7adb50191061c
pburyanov <p_buryanov-itssv@kw.com>
2024-01-22 20:15:01 +0200

    : wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const path = require('path')
-
-module.exports = {
-  entry: './src/index.ts',
-  resolve: { extensions: ['.ts', '.js', '.json'] },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: [/node_modules/, /old_files/, /devtools/],
-      },
-    ],
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: "source-map"
-}
+const path = require("path");
+module.exports = (_, options) => {
+  console.log(options)
+  return {
+    entry: path.resolve(__dirname, "src/index.ts"),
+    resolve: { extensions: [".ts", ".js", ".json"] },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: [/node_modules/, /old_files/, /devtools/],
+        },
+      ],
+    },
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devtool: "source-map",
+  };
+};
