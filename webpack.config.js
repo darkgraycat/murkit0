7177e6d3f5c7598b6cfbc9b01775761e77d403a8
pburyanov <p_buryanov-itssv@kw.com>
2024-01-22 19:50:02 +0200

    : wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
-//
 
 module.exports = (env) => {
   console.log(env);
@@ -15,26 +14,23 @@ module.exports = (env) => {
           use: "ts-loader",
           exclude: [ /node_modules/, /old_files/, /devtools/, /\.(?:test|spec)\.ts$/ ],
         },
-        {
-          test: [/\.html$/, /\.png$/],
-          loaders: [
-            'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
-            'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false'
-          ] 
-        }
+        // {
+        //   test: [/\.html$/, /\.png$/],
+        //   use: "file-loader",
+        // }
       ],
     },
-    devtool: "inline-source-map",
+    // devtool: "inline-source-map",
     devServer: { 
-      static: "./dist",
-      historyApiFallback: true
+      static: path.resolve(__dirname, "dist"),
+      // historyApiFallback: true
     },
     // plugins: [ new HtmlWebpackPlugin({ title: "Development", template: "./index.html" }) ],
     output: {
       filename: "[name].main.js",
       path: path.resolve(__dirname, "dist"),
       clean: true,
-      publicPath: "/",
+      publicPath: "./public",
     },
     optimization: {
       runtimeChunk: "single"
