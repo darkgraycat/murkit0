3cd82518dd0e7bc5e0223fd44fb3e8bb8e3c5b35
pburyanov <p_buryanov-itssv@kw.com>
2024-01-22 11:31:56 +0200

    : attempt0

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
-const path = require('path')
+const path = require("path");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+//
 
-module.exports = {
-  entry: './src/index.ts',
-  resolve: { extensions: ['.ts', '.js', '.json'] },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: [/node_modules/, /old_files/, /devtools/],
-      },
-    ],
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: "source-map"
-}
+module.exports = (env) => {
+  console.log(env);
+  return {
+    mode: "development",
+    entry: { index: "./src/index.ts" },
+    resolve: { extensions: [".ts", ".js", ".json"] },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: [ /node_modules/, /old_files/, /devtools/, /\.(?:test|spec)\.ts$/ ],
+        },
+        {
+          test: [/\.html$/, /\.png$/],
+          loaders: [
+            'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
+            'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false'
+          ] 
+        }
+      ],
+    },
+    devtool: "inline-source-map",
+    devServer: { 
+      static: "./dist",
+      historyApiFallback: true
+    },
+    // plugins: [ new HtmlWebpackPlugin({ title: "Development", template: "./index.html" }) ],
+    output: {
+      filename: "[name].main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      publicPath: "/",
+    },
+    optimization: {
+      runtimeChunk: "single"
+    }
+  };
+};
