b77a40f9e272609790cc70a0393ddf8a3ff623a6
Pavel Buryanov <jburyanov@gmail.com>
2024-02-26 05:47:43 +0200

    Refactor (#20)
    * : stage class
    
    * : wip
    
    * : .
    
    * : cleanup
    
    * : save my shitty-code before cleanup, just in case
    
    * : cleanup before refactor
    
    * : phew
    
    * : eto pizdec
    
    * : lookin good
    
    * : i believe i can move forward. easy easy, do not shoot own head... again
    
    * : huge win
    
    * : huge win
    
    * : demo
    
    * : uuuuuuuhhhhh html csssss
    
    * : uuuu
    
    ---------
    
    Co-authored-by: pburyanov <p_buryanov-itssv@kw.com>

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,11 @@ window.addEventListener("load", async () => {
   const overlay = document.getElementById("overlay") as any;
   overlay.set = (...msg: string[]) => (debugEl.innerHTML = msg.join("<br>"));
   overlay.add = (...msg: string[]) => (debugEl.innerHTML += msg.join("<br>"));
+  const buttons = [
+    document.getElementById("btn-left"),
+    document.getElementById("btn-right"),
+    document.getElementById("btn-jump"),
+  ];
 
   screen.width = width;
   screen.height = height;
@@ -26,26 +31,24 @@ window.addEventListener("load", async () => {
     KeyA: "Left", KeyD: "Right", Space: "Jump",
     ArrowLeft: "Left", ArrowRight: "Right", ArrowUp: "Jump",
   };
-  const mouseFields = {
-
-  }
   window.addEventListener("keydown", ({ code }) => actions.add(kbMapping[code] || code));
   window.addEventListener("keyup", ({ code }) => actions.delete(kbMapping[code] || code));
-  window.addEventListener("mousedown", () => {
-
-  })
-
-  setInterval(() => {
-    if (actions.size) console.log(actions);
-  }, 100);
-
-  // init({
-  //   screen,
-  //   width,
-  //   height,
-  //   keys,
-  //   fps: 1000 / 60,
-  // });
+  for (const btn of buttons) {
+    btn.addEventListener("mousedown", (e) => {
+      // @ts-ignore
+      const id = e.target.id;
+      if (id === "btn-left") actions.add("Left");
+      if (id === "btn-jump") actions.add("Jump");
+      if (id === "btn-right") actions.add("Right");
+    });
+    btn.addEventListener("mouseup", (e) => {
+      // @ts-ignore
+      const id = e.target.id;
+      if (id === "btn-left") actions.delete("Left");
+      if (id === "btn-jump") actions.delete("Jump");
+      if (id === "btn-right") actions.delete("Right");
+    });
+  }
 
   const game = await runnerGame({ screen, overlay, width, height, actions, fps: 1000/60 });
   game.engine.start();
